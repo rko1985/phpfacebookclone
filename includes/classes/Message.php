d918e8419a42e29f518fdf834c343baef0b709ae
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-12-26 09:38:07 -1000

    dropdown message box complete

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -222,12 +222,18 @@ class Message {
 
         //If posts were loaded
         if($count > $limit)
-            $return_string .= "<input type='hidden' class='nextPageDropDownData' value='" . ($page +1). "'><input type='hidden' class='noMoreDropdownData' value='false'>";
+            $return_string .= "<input type='hidden' class='nextPageDropdownData' value='" . ($page +1). "'><input type='hidden' class='noMoreDropdownData' value='false'>";
         else
             $return_string .= "<input type='hidden' class='noMoreDropdownData' value='true'> <p style='text-align: center;'>No more messages to load!</p>";
 
 
         return $return_string;
+    }
+
+    public function getUnreadNumber(){
+        $userLoggedIn = $this->user_obj->getUsername();
+        $query = mysqli_query($this->con, "SELECT * FROM messages WHERE viewed = 'no' AND user_to ='$userLoggedIn' ");
+        return mysqli_num_rows($query);
     }
 
 }
--- includes/header.php
+++ includes/header.php
@@ -42,6 +42,11 @@ if(isset($_SESSION['username'])){
         </div>
 
         <nav>
+            <?php 
+                //unread messages
+                $messages = new Message($con, $userLoggedIn);
+                $num_messages = $messages->getUnreadNumber();
+            ?>
             <a href="<?php echo $userLoggedIn;?>">
                 <?php echo $user['first_name']; ?>
             </a>
@@ -50,6 +55,10 @@ if(isset($_SESSION['username'])){
             </a>
             <a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
                 <i class="fa fa-envelope fa-lg"></i>
+                <?php 
+                if($num_messages > 0)
+                echo '<span class="notification_badge" id="unread_message">'. $num_messages .'</span>';
+                ?>
             </a>
             <a href="#">
                 <i class="fa fa-bell-o fa-lg"></i>
@@ -70,5 +79,53 @@ if(isset($_SESSION['username'])){
 
     </div>
 
+    <script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('.dropdown_data_window').scroll(function() {
+			var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+			var scroll_top = $('.dropdown_data_window').scrollTop();
+			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+
+			if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+
+				var pageName; //Holds name of page to send ajax request to
+				var type = $('#dropdown_data_type').val();
+
+
+				if(type == 'notification')
+					pageName = "ajax_load_notifications.php";
+				else if(type = 'message')
+					pageName = "ajax_load_messages.php"
+
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/" + pageName,
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
+
+
+						$('.dropdown_data_window').append(response);
+					}
+				});
+
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
+
+
+	});
+
+	</script>
     <div class="wrapper">
     
